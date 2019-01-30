49d8513fa8500f39e663795a01f35cfe57dbb9f6
Baltan <617640006@qq.com>
2019-01-30 16:24:33 +0800

    不再将服务提供者提供的访问URL配置在配置文件中，动态获取服务实例，解决了存在多个服务url的问题

--- ticket-consumer/src/main/java/com/baltan/consumer/controller/ConsumerController.java
+++ ticket-consumer/src/main/java/com/baltan/consumer/controller/ConsumerController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -26,13 +28,27 @@ public class ConsumerController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
-    @Value("${provider.url}")
-    private String url;
+//    @Value("${provider.url}")
+//    private String url;
 
     @GetMapping("/buyTicket/{name}")
     public String buyTicket(@PathVariable("name") String name) {
 
-        String ticketName = restTemplate.getForObject(url + "getTicket", String.class);
+//        String ticketName = restTemplate.getForObject(url + "getTicket", String.class);
+//        return name + "购买了" + ticketName;
+
+        String serviceId = "ticket-provider";
+        List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+        if (instances == null || instances.isEmpty()) {
+            return null;
+        }
+        ServiceInstance instance = instances.get(0);
+        String url = "http://" + instance.getServiceId() + "/getTicket";
+        /**
+         * 这种方式不用将服务url写进配置文件，解决了多个服务url的问题
+         * 不能直接访问服务的ip，只能访问服务注册在eureka注册中心的application.name
+         */
+        String ticketName = restTemplate.getForObject(url, String.class);
         return name + "购买了" + ticketName;
     }
 }
