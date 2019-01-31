84f098402679f077654e628d6ae057c152161759
Baltan <617640006@qq.com>
2019-01-31 16:56:54 +0800

    使用ribbon实现负载均衡，并添加测试用例

--- ticket-consumer/src/main/java/com/baltan/consumer/controller/ConsumerController.java
+++ ticket-consumer/src/main/java/com/baltan/consumer/controller/ConsumerController.java
@@ -38,12 +38,16 @@ public class ConsumerController {
 //        return name + "购买了" + ticketName;
 
         String serviceId = "ticket-provider";
-        List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
-        if (instances == null || instances.isEmpty()) {
-            return null;
-        }
-        ServiceInstance instance = instances.get(0);
-        String url = "http://" + instance.getServiceId() + "/getTicket";
+//        List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+//        if (instances == null || instances.isEmpty()) {
+//            return null;
+//        }
+//        ServiceInstance instance = instances.get(0);
+        /**
+         * RestTemplate Bean添加@LoadBalanced注解后，在调用getForObject()
+         * 方法时，会先根据传入的serviceId找到注册中心中所有可用的服务，再根据内部算法选择一个ip进行访问，实现负载均衡
+         */
+        String url = "http://" + serviceId + "/getTicket";
         /**
          * 这种方式不用将服务url写进配置文件，解决了多个服务url的问题
          * 不能直接访问服务的ip，只能访问服务注册在eureka注册中心的application.name
--- ticket-consumer/src/test/java/com/baltan/consumer/TicketConsumerApplicationTests.java
+++ ticket-consumer/src/test/java/com/baltan/consumer/TicketConsumerApplicationTests.java
@@ -2,15 +2,28 @@ package com.baltan.consumer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Import(value = {TicketConsumerApplication.class})
 public class TicketConsumerApplicationTests {
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @Test
-    public void contextLoads() {
+    public void loadBalanceTest() {
+        String serviceId = "ticket-provider";
+        for (int i = 0; i < 100; i++) {
+            ServiceInstance instance = loadBalancerClient.choose(serviceId);
+            System.out.println(instance.getUri());
+        }
     }
 
 }
