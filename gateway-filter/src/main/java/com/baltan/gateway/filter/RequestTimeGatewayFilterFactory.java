90ee26c3602bafcc1c0cb99fa7bdda6d0491d25f
Baltan <617640006@qq.com>
2019-03-03 16:48:29 +0800

    为Spring Cloud Gateway配置Filter

---
+++ gateway-filter/src/main/java/com/baltan/gateway/filter/RequestTimeGatewayFilterFactory.java
@@ -0,0 +1,67 @@
+package com.baltan.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Description: 自定义过滤器工厂
+ *              Reference: https://blog.csdn.net/forezp/article/details/85057268
+ *
+ * @author Baltan
+ * @date 2019-03-03 16:33
+ */
+public class RequestTimeGatewayFilterFactory extends
+        AbstractGatewayFilterFactory<RequestTimeGatewayFilterFactory.Config> {
+
+
+    private static final String REQUEST_TIME_BEGIN = "requestTimeBegin";
+    private static final String KEY = "withParams";
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList(KEY);
+    }
+
+    public RequestTimeGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            exchange.getAttributes().put(REQUEST_TIME_BEGIN, System.currentTimeMillis());
+            return chain.filter(exchange).then(
+                    Mono.fromRunnable(() -> {
+                        Long startTime = exchange.getAttribute(REQUEST_TIME_BEGIN);
+                        if (startTime != null) {
+                            StringBuilder sb = new StringBuilder(exchange.getRequest().getURI().getRawPath())
+                                    .append(": ")
+                                    .append(System.currentTimeMillis() - startTime)
+                                    .append("ms");
+                            if (config.isWithParams()) {
+                                sb.append(" params:").append(exchange.getRequest().getQueryParams());
+                            }
+                            System.out.println(sb.toString());
+                        }
+                    })
+            );
+        };
+    }
+
+    public static class Config {
+
+        private boolean withParams;
+
+        public boolean isWithParams() {
+            return withParams;
+        }
+
+        public void setWithParams(boolean withParams) {
+            this.withParams = withParams;
+        }
+    }
+}
