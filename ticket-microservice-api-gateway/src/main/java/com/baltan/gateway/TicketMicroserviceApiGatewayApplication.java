b4b26d227dde85e0863a66be05132dc0c9e8dcab
Baltan <617640006@qq.com>
2019-03-03 23:25:54 +0800

    通过Spring Cloud Gateway实现路由转发功能

---
+++ ticket-microservice-api-gateway/src/main/java/com/baltan/gateway/TicketMicroserviceApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.baltan.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMicroserviceApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketMicroserviceApiGatewayApplication.class, args);
+    }
+
+}
