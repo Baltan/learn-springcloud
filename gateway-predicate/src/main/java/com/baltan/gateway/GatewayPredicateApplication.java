6586daba904d24945e4c6e2bc047886faa726003
Baltan <617640006@qq.com>
2019-03-03 16:10:29 +0800

    为Spring Cloud Gateway配置Predicate

---
+++ gateway-predicate/src/main/java/com/baltan/gateway/GatewayPredicateApplication.java
@@ -0,0 +1,13 @@
+package com.baltan.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPredicateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayPredicateApplication.class, args);
+    }
+
+}
